534ae889fdf43de2803d5a1510722477ecdacee5
Shamanth Kumar Parameshwar <shamanth.parameshwar@xcelenergy.com>
2025-08-21 17:31:11 -0500

    Add Dockerfiles for frontend and server

--- server/routes/api/userMovies.js
+++ server/routes/api/userMovies.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const router = express.Router();
 
