4bd7b6175a33a30aaff68f1e0c343169a575d534
Shamanth Kumar Parameshwar <shamanth.parameshwar@xcelenergy.com>
2025-08-27 12:07:07 -0500

    Fix lint issues

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,8 +1,18 @@
 import js from "@eslint/js";
-import globals from "globals";
+// import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
 ]);
